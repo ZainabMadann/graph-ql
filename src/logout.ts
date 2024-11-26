a861d630f404e1157e2d9053ebc2ad60fe43d48f
ZainabMadann <zainabmadann@gmail.com>
2024-11-26 17:58:40 +0300

    logout

--- src/logout.ts
+++ src/logout.ts
@@ -1,6 +1,31 @@
 import { showLoginPage } from "./view/loginview"
 
-export function logoutHandler(){
-    localStorage.removeItem('jwt')
-    showLoginPage()
+// export function logoutHandler(){
+//     localStorage.removeItem('jwt')
+//     showLoginPage()
+// }
+export async function logoutHandler(){
+    const jwt = localStorage.getItem('jwt')
+
+    try {
+        const response = await fetch('https://learn.reboot01.com/api/auth/expire', {
+            
+            method: 'GET',
+            headers: {
+                'x-jwt-token': `${jwt}`,
+            }
+        });
+
+        localStorage.removeItem('jwt')
+        
+        if (!response.ok) {
+            throw new Error('Logout failed')
+        }
+
+        showLoginPage()
+    } catch (error) {
+        console.error('Error:', error);
+        alert('logout failed.')
+    }
+    
 }
